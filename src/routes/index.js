fd25eb822821172256e1d4f862a44572b2d2dcb1
luiscostalafc <luiscostalafc@gmail.com>
2020-07-14 20:48:30 -0300

    Remove SingUp

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SingIn from '../pages/SingIn';
-import SingUp from '../pages/SingUp';
+// import SingUp from '../pages/SingUp';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
@@ -13,7 +13,7 @@ export default function Routes() {
 	return (
 		<Switch>
 			<Route path="/" exact component={SingIn} />
-			<Route path="/register" component={SingUp} />
+			{/* <Route path="/register" component={SingUp} /> */}
 
 			<Route path="/dashboard" component={Dashboard} isPrivate />
 			<Route path="/profile" component={Profile} isPrivate />
